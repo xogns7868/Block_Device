907efb4eda8de2077524023bd47bc1fb3fcf91a8
xogns7868 <superman7868@naver.com>
2020-02-13 23:21:43 +0900

    Update blkparser.cpp

--- blkparser.cpp
+++ blkparser.cpp
@@ -14,7 +14,7 @@ int main() {
     vector<int> v_lba;
     while(!inFile.eof()){
         getline(inFile, buffer);
-        if(buffer[4] == 'R'){
+        if(buffer[4] == 'R' && buffer[8] == '5' && buffer[9] == '1' && buffer[10] == '2'){
             for(int i = 12; i<20; i++){
                 lba += buffer[i];
             }
@@ -24,10 +24,15 @@ int main() {
         }
     }
     int flag = 0;
+    int count = 0;
     for(int i = 0; i < v_lba.size(); i++){
-        if(v_lba[i+1] - v_lba[i] > 1){
+        if(v_lba[i + 1] - v_lba[i] > 1){
             cout<<flag * 512<<" "<<v_lba[flag]<<" "<<v_lba[i]<<" "<<v_lba[i] - v_lba[flag] + 1<<endl;
             flag = i + 1;
+            count++;
         }
+    }
+    if(count % 2 != 0){
+        cout<<v_lba.size() * 512<<" "<<v_lba[flag]<<" "<<v_lba.back()<<" "<<v_lba.back() - v_lba[flag] + 1<<endl;
     }
 }
